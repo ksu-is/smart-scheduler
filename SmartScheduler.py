8d942f9b182a31e1d0e2b579a0811c7f988010e6
tiriqt <ttayl185@students.kennesaw.edu>
2025-04-14 16:16:51 -0400

    Update SmartScheduler.py

--- SmartScheduler.py
+++ SmartScheduler.py
@@ -8,3 +8,7 @@ from Models import Widgets
 from Models import ScheduleWidgets, ScheduleView
 from UI import ColorMap
 from PyPulse import PulseInterface
+class MainApp(QtWidgets.QMainWindow, mainDesign.Ui_MainWindow):
+    def __init__(self):
+        super(self.__class__, self).__init__()
+        self.setupUi(self)
