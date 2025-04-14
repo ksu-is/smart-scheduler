d8bdf0f93a7c454e4bfa46c472c101c5a3664c46
tiriqt <ttayl185@students.kennesaw.edu>
2025-04-14 16:16:00 -0400

    Update SmartScheduler.py
    added more compatible calendar types

--- SmartScheduler.py
+++ SmartScheduler.py
@@ -5,3 +5,6 @@ import pickle
 from PyQt5 import QtWidgets
 from Designs import mainDesign
 from Models import Widgets
+from Models import ScheduleWidgets, ScheduleView
+from UI import ColorMap
+from PyPulse import PulseInterface
