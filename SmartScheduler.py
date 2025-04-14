6f7fdc1dab239839af551aea6dba96035ee193af
tiriqt <ttayl185@students.kennesaw.edu>
2025-04-14 16:17:16 -0400

    Update SmartScheduler.py

--- SmartScheduler.py
+++ SmartScheduler.py
@@ -12,3 +12,6 @@ class MainApp(QtWidgets.QMainWindow, mainDesign.Ui_MainWindow):
     def __init__(self):
         super(self.__class__, self).__init__()
         self.setupUi(self)
+ self.current_schedule_type = None
+        self.schedule = dict()
+        self.schedule_headers = []
